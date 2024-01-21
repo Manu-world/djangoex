194f18db34d7038d81a5f1f77c344f8624c7cedc
manu <manu.softwareengineer@gmail.com>
2024-01-21 16:55:03 +0000

    significant improvements

--- appex/urls.py
+++ appex/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path("", views.index, name='home'),
-    path("count", views.counter, name='count')
+    path("count", views.counter, name='count'),
+    path('register', views.register, name='register')
 ]
--- appex/views.py
+++ appex/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'home.html')
+    services = ['web design', 'web developer', 'product management', 'app development']
+    return render(request, 'index.html', {"services": services})
 
 def counter(request):
     words = request.POST.get("content")
     num_of_words = len(words.split())
     return render(request, 'count.html', {"content": num_of_words})
+
+
+def register(request):
+    
+    return render(request, 'register.html')
 
