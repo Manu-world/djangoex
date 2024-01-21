c85372ab96e97ec019b9305ee0169b3041cb276b
manu <manu.softwareengineer@gmail.com>
2024-01-21 17:37:59 +0000

    added register feature

--- appex/views.py
+++ appex/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Books
 
 # Create your views here.
 
@@ -13,6 +16,25 @@ def counter(request):
 
 
 def register(request):
-    
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        username= request.POST['username']
+        email = request.POST['email']
+        password= request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'This Email already exists. Try a different one')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('home')
+        else:
+            messages.info(request, 'Password mismatched!')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 
